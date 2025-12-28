11bc0b751d7d745dbed254a809230895966a18e8
이경용 <sleekydz86@icloud.com>
2025-12-28 23:40:51 +0900

    feat:코어닷 Init

---
+++ backend/emr-core/src/main/java/com/sleekydz86/core/EmrCoreApplication.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmrCoreApplication.class, args);
+    }
+
+}
---
+++ backend/emr-core/src/test/java/com/sleekydz86/core/EmrCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmrCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
