c7009bfa5120b06e86dec45714bd3be036432515
이경용 <sleekydz86@icloud.com>
2025-12-28 23:38:50 +0900

    feat: backend단 셋팅

---
+++ backend/src/test/java/com/sleekydz86/emr/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleekydz86.emr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
