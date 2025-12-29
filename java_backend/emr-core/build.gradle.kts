ea9257782582654b9c8e51eff8f17ee8037fa8dd
이경용 <sleekydz86@icloud.com>
2025-12-29 23:21:45 +0900

    fix: 프로젝트 수정 1

--- java_backend/emr-core/build.gradle.kts
+++ java_backend/emr-core/build.gradle.kts
@@ -1,29 +1,21 @@
-plugins {
-    java
-    id("org.springframework.boot") version "4.0.1"
-    id("io.spring.dependency-management") version "1.1.7"
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+tasks.withType<BootJar> {
+    enabled = false
 }
 
-group = "com.sleekydz86"
-version = "0.0.1-SNAPSHOT"
-description = "emr-core"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-repositories {
-    mavenCentral()
+tasks.withType<Jar> {
+    enabled = true
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
+    api("io.github.microutils:kotlin-logging-jvm:2.0.11")
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    // jasypt
+    api("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.5")
+
+    // jwt
+    api("io.jsonwebtoken:jjwt-api:0.12.6")
+    api("io.jsonwebtoken:jjwt-impl:0.12.6")
+    api("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
