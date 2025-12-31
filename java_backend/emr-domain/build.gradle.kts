87d5c6b35fe3bd23beae9a60ceefe5d14731c0a8
이경용 <sleekydz86@icloud.com>
2025-12-31 23:16:44 +0900

    fix: 의존성 수정

--- java_backend/emr-domain/build.gradle.kts
+++ java_backend/emr-domain/build.gradle.kts
@@ -1,29 +1,11 @@
 plugins {
-    java
-    id("org.springframework.boot") version "4.0.1"
-    id("io.spring.dependency-management") version "1.1.7"
-}
-
-group = "com.sleekydz86"
-version = "0.0.1-SNAPSHOT"
-description = "emr-domain"
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
-repositories {
-    mavenCentral()
+    `java`
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(project(":emr-core"))
+    implementation("org.modelmapper:modelmapper:3.1.1")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
