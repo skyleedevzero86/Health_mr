e12a1936422ca53e7c9cc0bb8972d13bf5f27797
이경용 <sleekydz86@icloud.com>
2026-01-05 00:18:58 +0900

    feat: 검사 일지 관련 Entity 추가
    - 검사 일지 검사 수행 기록 관리 추가
    - 검사 일정 검사 일정 관리 추가
    - 검사 결과 검사 결과 관리 추가
    - 혈액은행 혈액 검사 관련 정보 관리 추가
    - 검사 일지 환자별, 검사별, 진료별, 사용자별, 장비별, 시간 범위별 조회 기능 추가

---
+++ java_backend/emr-support/src/main/java/com/sleekydz86/support/examination/entity/ExaminationJournalEntity.java
@@ -0,0 +1,57 @@
+package com.sleekydz86.support.examination.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sleekydz86.domain.common.entity.BaseEntity;
+import com.sleekydz86.domain.patient.entity.PatientEntity;
+import com.sleekydz86.domain.user.entity.UserEntity;
+import com.sleekydz86.emrclinical.treatment.entity.TreatmentEntity;
+import com.sleekydz86.support.equipment.entity.EquipmentEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity(name = "Examination_Journal")
+@Table(name = "examination_journal")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExaminationJournalEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "examination_journal_id", nullable = false)
+    private Long examinationJournalId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "examination_id", referencedColumnName = "examination_id", nullable = false)
+    private ExaminationEntity examinationEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_no", referencedColumnName = "patient_no", nullable = false)
+    private PatientEntity patientEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "treatment_id", referencedColumnName = "treatment_id", nullable = false)
+    private TreatmentEntity treatmentEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "equipment_id", referencedColumnName = "equipment_id", nullable = false)
+    private EquipmentEntity equipmentEntity;
+
+    @Column(name = "examination_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime examinationTime;
+
+    @Column(name = "examination_equipment_usage")
+    private Boolean examinationEquipmentUsage;
+
+    @Column(name = "examination_notes", columnDefinition = "TEXT")
+    private String examinationNotes;
+}
+
